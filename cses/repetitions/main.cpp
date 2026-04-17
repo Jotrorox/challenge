7d891b1fd30b2d0ea933580ed671f9a5e9774215
Pizzazium-Infinionite <147343451+Pizzazium-Infinionite@users.noreply.github.com>
2026-04-17 08:07:00 +0200

    optimised the repetition c++ program

--- cses/repetitions/main.cpp
+++ cses/repetitions/main.cpp
@@ -11,8 +11,23 @@ int main() {
     size_t max = 1;
     size_t cur = 1;
     for (size_t i = 1; i < input.length(); i++) {
-        cur = (input[i] == input[i-1]) ? cur + 1 : 1;
-        max = (cur > max) ? cur : max;
+        if(input[i] == input[i-1]){
+            cur ++;
+        }else{
+            cur = 1;
+            
+            size_t edge = i+max - 1;
+            if(edge <= input.length()){
+
+                if (input[edge] != input[edge-1]){
+                    i = edge;
+                }
+            }
+            
+        }
+        if(cur > max){
+            max = cur;
+        }
     }
 
     cout << max << "\n";
